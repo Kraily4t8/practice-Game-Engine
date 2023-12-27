62d78fa307c6ad5a201fc65c297e7def32800c43
Kraily4t8 <tomtien2002@outlook.com>
2023-12-26 18:13:48 -0800

    Update main.js
    charakter spritesheet loaded

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const ASSET_MANAGER = new AssetManager();
 
 //ex 
 //ASSET_MANAGER.queueDownload("/sprites/img.png");
+//charakter spritesheet loaded
 ASSET_MANAGER.queueDownload("/assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
 
 
