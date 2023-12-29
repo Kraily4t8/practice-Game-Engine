ce1d6a584d6343a6553609b932f7e5dc771e1865
Kraily4t8 <tomtien2002@outlook.com>
2023-12-29 00:39:57 -0800

    walkright2 animation

--- catplayer.js
+++ catplayer.js
@@ -4,6 +4,10 @@ class CatPlayer {
         this.spritesheet = ASSET_MANAGER.getAsset("/assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
         this.elapsedTime = 0;
         this.frame = 1;
+
+        this.cellStart = 18;
+        this.cellJump = 48;
+        this.iteration = 0;
     }
 
     update() {
@@ -13,15 +17,19 @@ class CatPlayer {
     draw(ctx) { //render
         this.elapsedTime++;
         this.walkRight(ctx,50,50);
+        this.walkRight2(ctx,200,50);
+        if(this.elapsedTime > 8) {
+            this.elapsedTime = 0;
+        }
     }
 
     walkRight(ctx,x,y) {
         if(this.elapsedTime > 8) {
-            this.elapsedTime = 0;
             this.frame++;
-            if (this.frame > 2) this.frame = 1;
+            if (this.frame > 2) this.frame = 0;
         }
-        // ctx = this.game.ctx;
+        //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+        //
         if(this.frame == 0) {
             ctx.drawImage(this.spritesheet, 18, 160, 20, 20, x, y, 100, 100);
         } else if (this.frame == 1){
@@ -32,6 +40,15 @@ class CatPlayer {
             ctx.drawImage(this.spritesheet, 163, 160, 20, 20, x, y, 100, 100);
         }
     }
+
+    walkRight2(ctx,x,y) {
+        ctx.drawImage(this.spritesheet, this.cellStart + this.cellJump * this.iteration, 160, 20, 20, x, y, 100,100);
+        if(this.elapsedTime > 8) {
+            this.iteration++;
+            if(this.iteration > 3) this.iteration = 0;
+        }
+        
+    }
     // animatCell(image, startPosition, cellsize,) {
     //     console.log("potato");
     // }
@@ -40,5 +57,5 @@ class CatPlayer {
     //cellsize
     //wavelength
     //gridify
-    //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+    
 };
