1f4fa9b334526252b7cdee6a1b35d0e231cdae15
Kraily4t8 <53635947+Kraily4t8@users.noreply.github.com>
2024-01-04 11:47:33 -0800

    marriot animator

--- catplayer.js
+++ catplayer.js
@@ -1,16 +1,12 @@
 class CatPlayer {
     constructor(game, x, y) {
         Object.assign(this, { game, x, y });
-        this.spritesheet = ASSET_MANAGER.getAsset("/assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
-        this.elapsedTime = 0;
-        this.frame = 1;
-
-        this.right = false;
-
-        this.cellStart = 18;
-        this.cellJump = 48;
-        this.iteration = 0;
-        this.looptime = 13;
+        this.spritesheet = ASSET_MANAGER.getAsset("./assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
+        //spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop
+        this.walkRight = new Animator(this.spritesheet, 67, 160, 11, 16, 2, 0.4, 37, false, true);
+        this.runRight = new Animator(this.spritesheet, 67, 160, 11, 16, 3, 0.3, 37, false, true);
+        this.walkX = 10;
+        this.runX = 10;
     }
 
     update() {
@@ -18,46 +14,25 @@ class CatPlayer {
     }
 
     draw(ctx) { //render
-        this.elapsedTime++;
         // if(this.game.keys[right.key]) {
         //     console.log("stuff");
         // }
-        this.walkRight(ctx,50,50);
-        this.walkRight2(ctx,200,50);
-        if(this.elapsedTime > this.looptime) {
-            this.elapsedTime = 0;
-        }
-    }
-
-    walkRight(ctx,x,y) {
-        if(this.elapsedTime > this.looptime) {
-            this.frame++;
-            if (this.frame > 2) this.frame = 0;
-        }
-        //drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
-        //
-        if(this.frame == 0) {
-            ctx.drawImage(this.spritesheet, 18, 160, 20, 20, x, y, 100, 100);
-        } else if (this.frame == 1){
-            ctx.drawImage(this.spritesheet, 67, 160, 20, 20, x, y, 100, 100);
-        } else if (this.frame == 2){
-            ctx.drawImage(this.spritesheet, 115, 160, 20, 20, x, y, 100, 100);
-        } else {
-            ctx.drawImage(this.spritesheet, 163, 160, 20, 20, x, y, 100, 100);
-        }
+        //drawFrame(tick, ctx, x, y, scale)
+        this.walkX += 0.2;
+        this.runX += 0.4;
+        this.walkRight.drawFrame(this.game.clockTick, ctx, this.walkX, 10, 3);
+        this.runRight.drawFrame(this.game.clockTick, ctx, this.runX, 80, 3);
+        // this.walkRight2(ctx,200,50);
     }
 
     walkRight2(ctx,x,y) {
-        ctx.drawImage(this.spritesheet, this.cellStart + this.cellJump * this.iteration, 160, 20, 20, x, y, 100,100);
+        ctx.drawImage(this.spritesheet, this.cellStart + this.cellJump * this.iteration, 160, 20, 20, x, y, 60,60);
         if(this.elapsedTime > this.looptime) {
             this.iteration++;
             if(this.iteration > 3) this.iteration = 0;
         }
         
     }
-    // animatCell(image, startPosition, cellsize,) {
-    //     console.log("potato");
-    // }
     //animate
     //selectcell
     //cellsize
