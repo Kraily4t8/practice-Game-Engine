5c40bdd743e9c006c01319d0111389a663625da8
Kraily4t8 <tomtien2002@outlook.com>
2024-01-24 16:50:35 -0800

    file clean up and organization

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ const ASSET_MANAGER = new AssetManager();
 //ASSET_MANAGER.queueDownload("/sprites/img.png");
 //charakter spritesheet loaded
 ASSET_MANAGER.queueDownload("./assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png");
-ASSET_MANAGER.queueDownload("./miku spritesheet.png");
-ASSET_MANAGER.queueDownload("./specter knight.png");
+ASSET_MANAGER.queueDownload("./assets/miku spritesheet.png");
+ASSET_MANAGER.queueDownload("./assets/specter knight.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -22,10 +22,10 @@ ASSET_MANAGER.downloadAll(() => {
 	//scenemanager
 	// gameEngine.addEntity(new CatPlayer(gameEngine, 0, 0, ASSET_MANAGER.getAsset("./assets/Sprout Lands - Sprites - Basic pack/Characters/Basic Charakter Spritesheet.png")));
 	
-	gameEngine.addEntity(new SpecterKnight(gameEngine, 150, 250, ASSET_MANAGER.getAsset("./specter knight.png")));
-	gameEngine.addEntity(new Ground(gameEngine, 10, 350, 750, ASSET_MANAGER.getAsset("./specter knight.png")));
+	gameEngine.addEntity(new SpecterKnight(gameEngine, 150, 250, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
+	gameEngine.addEntity(new Ground(gameEngine, 10, 350, 750, ASSET_MANAGER.getAsset("./assets/specter knight.png")));
 	// gameEngine.addEntity(new Ground(gameEngine, 300, 300, 120, ASSET_MANAGER.getAsset("./specter knight.png")));
 
-	gameEngine.addEntity(new Miku(gameEngine, 50, 50, ASSET_MANAGER.getAsset("./miku spritesheet.png")));
+	gameEngine.addEntity(new Miku(gameEngine, 50, 50, ASSET_MANAGER.getAsset("./assets/miku spritesheet.png")));
 	gameEngine.start();
 });
